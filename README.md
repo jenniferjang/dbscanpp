033ec0519faf9143f902e5360b2033c5ab36780c
Jennifer Jang <j.jang42@gmail.com>
2018-04-09 00:59:42 -0700

    README

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# CoreSetsMeanshift
+# CoreSets
--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from Cython.Distutils import build_ext
 
 
 setup(
-    name='CoreSetsMeanshift',
+    name='CoreSets',
     version='1.0',
     cmdclass={'build_ext': build_ext},
-    ext_modules=[Extension("CoreSetsMeanshift",
-                 sources=["core_sets_meanshift.pyx"],
+    ext_modules=[Extension("CoreSets",
+                 sources=["core_sets.pyx"],
                  language="c++",
                  include_dirs=[numpy.get_include()])],
     author='Jennifer Jang',
