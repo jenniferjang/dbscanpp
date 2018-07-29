96af648af809d2fa82e8474d233af1ac7db01fde
Jennifer Jang <j.jang42@gmail.com>
2018-07-29 11:59:29 -0700

    Comments and docstrings

--- DBSCAN.h
+++ DBSCAN.h
@@ -10,8 +10,8 @@ using namespace std;
 
 void DBSCAN_cy(int c, int n,
                int * X_core,
-               int * neighbors,
-               int * neighbors_ind,
+               int * neighbors_pts,
+               int * num_neighbors,
                int * result) {
     /*
         Cluster the core points using density-based methods
@@ -20,18 +20,17 @@ void DBSCAN_cy(int c, int n,
         ----------
         c: Number of core points
         n: Size of original dataset
-        minPts: Minimum number of neighbors to be considered core point
         X_core: (c, ) array of the indices of the core points
         neighbors: array of indices to each neighbor within eps_clustering
                    distance for all core points
-        neighbors_ind: (c, ) number of neighbors for each core point, to
+        num_neighbors: (c, ) number of neighbors for each core point, to
                        be used for indexing into neighbors array
         result: (n, ) array of cluster results to be calculated
     */
 
     // these literally don't work?
     assert(X_core.size() == c);
-    assert(neighbors_ind.size() == c);
+    assert(num_neighbors.size() == c);
     assert(result.size() == n);
 
     queue<int> q = queue<int>();
@@ -48,9 +47,9 @@ void DBSCAN_cy(int c, int n,
 
                 start_ind = 0;
                 if (point != 0) {
-                    start_ind = neighbors_ind[point - 1];
+                    start_ind = num_neighbors[point - 1];
                 }
-                end_ind = neighbors_ind[point];
+                end_ind = num_neighbors[point];
 
                 for (int j = start_ind; j < end_ind; j++) {
                     neighbor = neighbors[j];
--- core_sets.pyx
+++ core_sets.pyx
@@ -3,12 +3,10 @@ cimport numpy as np
 import random
 import math
 from sklearn.neighbors import KDTree
-from sklearn.neighbors.kde import KernelDensity
-from sklearn.metrics.pairwise import pairwise_distances, euclidean_distances
 from datetime import datetime
 
-cdef extern from "k_init.h":
-    void k_init_cy2(int m, int n, int d,
+cdef extern from "k_centers_cy.h":
+    void k_centers_cy(int m, int n, int d,
                       double * X,
                       double * closest_dist_sq,
                       int * result)
@@ -18,7 +16,7 @@ cdef k_centers_np(m, n, d,
                   np.ndarray[double,  ndim=2, mode="c"] X,
                   np.ndarray[double, ndim=1, mode="c"] closest_dist_sq,
                   np.ndarray[np.int32_t, ndim=1, mode="c"] result):
-    k_init_cy2(m, n, d,
+    k_centers_cy(m, n, d,
                  <double *> np.PyArray_DATA(X),
                  <double *> np.PyArray_DATA(closest_dist_sq),
                  <int *> np.PyArray_DATA(result))
@@ -43,27 +41,6 @@ cdef DBSCAN_np(c, n,
               <int *> np.PyArray_DATA(result))
 
 
-cdef extern from "quickshift.h":
-    void quickshift_cy(int m, int n,
-                       double kernel_size,
-                       int * X_sampled,
-                       double * distances,
-                       double * densities,
-                       int * result)
-
-
-cdef quickshift_np(m, n, kernel_size,
-                   np.ndarray[int, ndim=1, mode="c"] X_sampled,
-                   np.ndarray[double, ndim=2, mode="c"] distances,
-                   np.ndarray[double, ndim=1, mode="c"] densities,
-                   np.ndarray[np.int32_t, ndim=1, mode="c"] result):
-    quickshift_cy(m, n, kernel_size,
-                  <int *> np.PyArray_DATA(X_sampled),
-                  <double *> np.PyArray_DATA(distances),
-                  <double *> np.PyArray_DATA(densities),
-                  <int *> np.PyArray_DATA(result))
-
-
 cdef extern from "cluster_remaining.h":
     void cluster_remaining_cy(int n,
                               int * closest_point,
@@ -79,7 +56,27 @@ cdef cluster_remaining_np(n,
 
 class CoreSetsDBSCAN:
     """
+    Parameters
+    ----------
     
+    p: The sample multiplier, which controls m, the number of points to sample
+
+    eps_density: Radius for determining core points; points that have greater than
+                 minPts in its eps_density-radii ball will be considered core points
+
+    eps_clustering: Radius for determining neighbors and edges in the density graph
+
+    minPts: Number of neighbors required for a point to be labeled a core point. Works
+            in conjunction with eps_density
+
+
+    Attributes
+    ----------
+
+    n_clusters: number of clusters fitted
+
+    cluster_map: a map from the cluster (zero-based indexed) to the list of points
+        in that cluster
 
     """
 
@@ -91,6 +88,24 @@ class CoreSetsDBSCAN:
         self.minPts = minPts
 
     def k_centers(self, m, n, d, X):
+      """
+      Return m points from X that are far away from each other
+
+      Parameters
+      ----------
+      m: Number of points to sample
+      n: Size of original dataset
+      d: Dimensions of original dataset
+      X: (m, d) dataset
+      closest_dist_sq: (n, ) empty array to be filled with the minimum
+                       distance to any points selected thus far
+      result: (m, ) indices of the m selected points in X
+
+      Returns
+      ----------
+      (m, ) list of indices
+      """
+
       n, d = X.shape
 
       indices = np.empty(m, dtype=np.int32)
@@ -103,36 +118,46 @@ class CoreSetsDBSCAN:
 
       return indices
 
-    def fit_predict(self, X, sample=True, init="k-means++", technique="exponential"):
+    def fit_predict(self, X, init="k-centers"):
         """
+        Determines the clusters in three steps.
+        First step is to sample points from X using either the
+        k-centers greedy sampling technique or a uniform
+        sample technique. The next step is to run DBSCAN on the
+        sampled points using the k-NN densities. Finally, all the 
+        remaining points are clustered to the closest cluster.
+
+        Parameters
+        ----------
+        X: Data matrix. Each row should represent a datapoint in 
+           euclidean space
+        init: String. Either "k-centers" for the k-centers greedy
+              sampling technique or "uniform" for a uniform random
+              sampling technique.
+
+        Returns
+        ----------
+        (n, ) cluster labels
         """
 
         X = np.ascontiguousarray(X)
         n, d = X.shape
-        if sample:
-          if technique == "exponential":
-            m = int(self.p * math.pow(n, d/(d + 4.0)))
-          elif technique == "linear":
-            m = int(self.p * n)
-          else:
-            raise ValueError("Technique %s is not defined." % technique)
-          if m < 1: 
-            raise ValueError("p is too small, so sampling did not produce any points.")
+        m = int(self.p * math.pow(n, d/(d + 4.0)))
 
-          # Find a random subset of m points 
-          if init == "k-means++":
-            X_sampled = self.k_centers(m, n, d, X)
-          else:
-            X_sampled = np.random.choice(np.arange(n, dtype=np.int32), m, replace=False)
-          X_sampled_pts = X[X_sampled]
+        # Find a random subset of m points 
+        if init == "k-centers":
+          X_sampled_ind = self.k_centers(m, n, d, X)
+        elif init == "uniform":
+          X_sampled_ind = np.random.choice(np.arange(n, dtype=np.int32), m, replace=False)
         else:
-          X_sampled = np.arange(n, dtype=np.int32)
-          X_sampled_pts = X
+          raise ValueError("Initialization technique %s is not defined." % init)
+        X_sampled_pts = X[X_sampled_ind]
 
         # Find the core points
-        radii = KDTree(X).query(X_sampled_pts, k=self.minPts)[0]
-        X_core = X_sampled[radii[:,-1] <= self.eps_density]
-        X_core_pts = X[X_core]
+        X_tree = KDTree(X)
+        radii = X_tree.query(X_sampled_pts, k=self.minPts)[0]
+        X_core_ind = X_sampled_ind[radii[:,-1] <= self.eps_density]
+        X_core_pts = X[X_core_ind]
 
         # Get the list of core neighbors for each core point
         core_pts_tree = KDTree(X_core_pts)
@@ -141,68 +166,23 @@ class CoreSetsDBSCAN:
         neighbors = core_pts_tree.query_radius(X_core_pts, eps_clustering)
         neighbors = np.asarray(np.concatenate(neighbors), dtype=np.int32)
         neighbors_ct = core_pts_tree.query_radius(X_core_pts, eps_clustering, count_only=True)
-        neighbors_ind = np.cumsum(neighbors_ct, dtype=np.int32)
+        num_neighbors = np.cumsum(neighbors_ct, dtype=np.int32)
         
         # Cluster the core points
-        c = X_core.shape[0]
+        c = X_core_ind.shape[0]
         result = np.full(n, -1, dtype=np.int32)
         DBSCAN_np(c,
                   n,
-                  X_core,
+                  X_core_ind,
                   neighbors,
-                  neighbors_ind,
+                  num_neighbors,
                   result)
 
         # Find the closest core point to every data point
         closest_core_pts = core_pts_tree.query(X, k=1)[1]
-        closest = X_core[closest_core_pts[:,0]]
+        closest = X_core_ind[closest_core_pts[:,0]]
 
         # Cluster the remaining points
         cluster_remaining_np(n, closest, result)
-
-        return result
-
-
-class CoreSetsMeanshift:
-    """
-
-    """
-
-    def __init__(self, p, kernel_size, kernel="gaussian"):
-        self.p = p
-        self.kernel_size = kernel_size
-        self.kernel = kernel
-
-
-    def fit_predict(self, X):
-        """
-        
-        """
-        X = np.array(X)
-        n, d = X.shape 
-        m = int(self.p * math.pow(n, d/(d + 4.0)))
-
-        if m < 1:
-          raise ValueError("p is too small, so sampling did not produce any points.")
-
-        # Find a random subset of m points 
-        X_sampled = np.random.choice(np.arange(n, dtype=np.int32), m, replace=False)
-        X_sampled.sort()
-        X_sampled_pts = X[X_sampled]
-        distances = pairwise_distances(X_sampled_pts, X_sampled_pts)
-
-        kde = KernelDensity(kernel=self.kernel, bandwidth=self.kernel_size).fit(X)
-        densities = kde.score_samples(X_sampled_pts)
-
-        result = np.full(n, -1, dtype=np.int32)
-        quickshift_np(m, n, self.kernel_size, X_sampled, distances, densities, result)
-
-        # Find the closest core point to every data point
-        tree = KDTree(X_sampled_pts)
-        closest_point = tree.query(X, k=1)[1]
-        closest_point = X_sampled[closest_point[:,0]]
-
-        # Cluster the remaining points
-        cluster_remaining_np(n, closest_point, result)
 
         return result
